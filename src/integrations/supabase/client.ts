32c81a6ca1c72c7d917c23bf8a90de7ed6ff2ab6
Giiid <gideonfummey1@gmail.com>
2025-10-13 10:55:57 +0100

    Remove api key and supabase url from code

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,9 +2,8 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = "https://xuvrmdukjkgqrbqpsunf.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh1dnJtZHVramtncXJicXBzdW5mIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM3NTMyMzEsImV4cCI6MjA2OTMyOTIzMX0.CaGGAoboD4GqGm3fkqEQkGz-uxEUa-p1fJFHM1DtZ0Q";
-
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_PUBLISHABLE_KEY = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
 
